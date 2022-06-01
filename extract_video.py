ce95f287c59159fd396e3e2d87d93ae9a833cfa3
Francis <francis@physiotec.org>
2022-06-01 19:32:47 -0400

    Add stop button and changed exported file names from frames to time

--- extract_video.py
+++ extract_video.py
@@ -2,6 +2,9 @@ from pathlib import Path
 import cv2 as cv
 import tkinter as tk
 from tkinter import W, ttk, filedialog, messagebox
+import math
+
+from cv2 import CAP_PROP_POS_FRAMES
 
 cap = 0
 step = 0
@@ -44,11 +47,16 @@ def openFile():
     if not cap.isOpened():
         messagebox.showerror('Erreur', 'Impossible d\'ouvrir le fichier')
         return False
+
+    start_time.set("0")
+    end_time.set(math.trunc(cap.get(cv.CAP_PROP_FRAME_COUNT) / cap.get(cv.CAP_PROP_FPS)) + 1)
     
     return True
 
 def runExtraction():
     global in_progress, destination_directory, filename, start_frame, end_frame, cap
+    if not cap.isOpened():
+        return
     in_progress = True
 
     #Retrieve the filename
@@ -62,7 +70,7 @@ def runExtraction():
     else:
         destination_directory = destination_path.get()
     
-    is_time_valid, debut, fin = validateTimes(start_time.get(), end_time.get(), cap.get(cv.CAP_PROP_FRAME_COUNT) / cap.get(cv.CAP_PROP_FPS))
+    is_time_valid, debut, fin = validateTimes(start_time.get(), end_time.get())
     if not is_time_valid:
         messagebox.showerror("Temps invalide", "Les temps entrés ne sont pas valides")
         return
@@ -70,8 +78,15 @@ def runExtraction():
     # Set the start frame
     cap.set(cv.CAP_PROP_POS_FRAMES, debut * cap.get(cv.CAP_PROP_FPS))
     start_frame = cap.get(cv.CAP_PROP_POS_FRAMES)
-    end_frame = fin * cap.get(cv.CAP_PROP_FPS)
+    end_frame = min(fin * cap.get(cv.CAP_PROP_FPS), cap.get(cv.CAP_PROP_FRAME_COUNT))
     extract()
+
+def stopExtraction():
+    global in_progress
+    in_progress = False
+    setWidgetWhileExtracting(in_progress)
+    progress_label.set("")
+    progressbar['value'] = 0
 
 def extract():
     global in_progress, destination_directory, filename, start_frame, end_frame
@@ -85,27 +100,54 @@ def extract():
         in_progress = False
         done = True
 
-    if in_progress and not cv.imwrite(destination_directory + "/" + filename + "_frame" + str(cap.get(cv.CAP_PROP_POS_FRAMES)).split('.')[0] + ".jpg", frame):
-        in_progress = False
+    min, sec, ms = msec_to_minute_seconde_msec(round(cap.get(cv.CAP_PROP_POS_FRAMES) / cap.get(cv.CAP_PROP_FPS) * 1000))
+    image_name = destination_directory + "/" + filename + "_" + min + "m_" + sec + "s_" + ms + "ms" + ".jpg"
+    if not Path(image_name).is_file():
+        if in_progress and not cv.imwrite(image_name, frame):
+            in_progress = False
 
     progress_label.set(str(round(cap.get(cv.CAP_PROP_POS_FRAMES) - start_frame)) + "/" + str(round(end_frame - start_frame)))
     progressbar['value'] = round((cap.get(cv.CAP_PROP_POS_FRAMES) - start_frame) / (end_frame - start_frame) * 100)
 
+
     if done:
-        messagebox.showinfo("Terminé!", "L'extraction est terminée")
         cap.release()
         in_progress = False
 
-    root.after(2, extract)
+    setWidgetWhileExtracting(in_progress)
+    root.after(20, extract)
 
-def validateTimes(debut, fin, max):
+def msec_to_minute_seconde_msec(time):
+    timeleft = time
+    min = math.floor(timeleft / 60000)
+    timeleft -= min * 60000
+    sec = math.floor(timeleft / 1000)
+    timeleft -= sec * 1000
+    return (str(min), '{:0>2}'.format(sec), '{:0>4}'.format(timeleft))
+
+
+def setWidgetWhileExtracting(isExtracting):
+    if isExtracting:
+        start_time_entry.config(state="disabled")
+        end_time_entry.config(state="disabled")
+        previous_step_button.config(state="disabled")
+        start_extraction_button.config(state="disabled")
+        stop_extraction_button.grid(row=7, column=1, **default_options)
+    else:
+        start_time_entry.config(state="normal")
+        end_time_entry.config(state="normal")
+        previous_step_button.config(state="normal")
+        start_extraction_button.config(state="normal")
+        stop_extraction_button.grid_forget()
+
+def validateTimes(debut, fin):
     try:
         debut = int(debut)
         fin = int(fin)
     except:
         return (False, debut, fin)
 
-    if (debut < 0 or fin < 1 or fin > max or debut == fin or fin < debut):
+    if (debut < 0 or fin < 1 or debut == fin or fin < debut):
         return (False, debut, fin)
     return (True, debut, fin)
 
@@ -144,34 +186,39 @@ file_destination_browse_button.grid(row=4, column=1, **default_options)
 next_step_button = ttk.Button(window_step1, text="Suivant", command=nextStep).grid(row=5, columnspan=3, **default_options)
 
 # Window 2 components
-ttk.Label(window_step2, text="Nom du fichier:").grid(row=0, column=0, sticky=W)
+ttk.Label(window_step2, text="Nom du fichier:").grid(row=0, column=0, sticky=W, **default_options)
 info_file_name_variable = tk.StringVar()
-ttk.Label(window_step2, textvariable=info_file_name_variable).grid(row=0, column=1, sticky=W)
+ttk.Label(window_step2, textvariable=info_file_name_variable).grid(row=0, column=1, sticky=W, **default_options)
 
-ttk.Label(window_step2, text="FPS:").grid(row=1, column=0, sticky=W)
+ttk.Label(window_step2, text="FPS:").grid(row=1, column=0, sticky=W, **default_options)
 info_fps_variable = tk.StringVar()
-ttk.Label(window_step2, textvariable=info_fps_variable).grid(row=1, column=1, sticky=W)
+ttk.Label(window_step2, textvariable=info_fps_variable).grid(row=1, column=1, sticky=W, **default_options)
 
-ttk.Label(window_step2, text="Durée:").grid(row=2, column=0, sticky=W)
+ttk.Label(window_step2, text="Durée:").grid(row=2, column=0, sticky=W, **default_options)
 info_duree_variable = tk.StringVar()
-ttk.Label(window_step2, textvariable=info_duree_variable).grid(row=2, column=1, sticky=W)
+ttk.Label(window_step2, textvariable=info_duree_variable).grid(row=2, column=1, sticky=W, **default_options)
 
-ttk.Label(window_step2, text="Temps de début (s):").grid(row=3, column=0, sticky=W)
+ttk.Label(window_step2, text="Temps de début (s):").grid(row=3, column=0, sticky=W, **default_options)
 start_time = tk.StringVar()
-ttk.Entry(window_step2, textvariable=start_time, width=4).grid(row=3, column=1, sticky=W)
+start_time_entry = ttk.Entry(window_step2, textvariable=start_time, width=4)
+start_time_entry.grid(row=3, column=1, sticky=W, **default_options)
 
-ttk.Label(window_step2, text="Temps de fin (s):").grid(row=4, column=0, sticky=W)
+ttk.Label(window_step2, text="Temps de fin (s):").grid(row=4, column=0, sticky=W, **default_options)
 end_time = tk.StringVar()
-ttk.Entry(window_step2, textvariable=end_time, width=4).grid(row=4, column=1, sticky=W)
+end_time_entry = ttk.Entry(window_step2, textvariable=end_time, width=4)
+end_time_entry.grid(row=4, column=1, sticky=W, **default_options)
 
 progressbar = ttk.Progressbar(window_step2, length=250, mode="determinate")
 progressbar.grid(row=5, columnspan=3, **default_options)
 progress_label = tk.StringVar()
 ttk.Label(window_step2, textvariable=progress_label).grid(row=6, columnspan=3)
 
-previous_step_button = ttk.Button(window_step2, text="Précédent", command=previousStep).grid(row=7, column=0, **default_options)
-start_extraction_button = ttk.Button(window_step2, text="Extraire", command=runExtraction).grid(row=7, column=1, **default_options)
+previous_step_button = ttk.Button(window_step2, text="Précédent", command=previousStep)
+previous_step_button.grid(row=7, column=0, **default_options)
+start_extraction_button = ttk.Button(window_step2, text="Extraire", command=runExtraction)
+start_extraction_button.grid(row=7, column=1, **default_options)
 
+stop_extraction_button = ttk.Button(window_step2, text="Arrêter", command=stopExtraction)
 
 window_step1.grid(padx=10, pady=10)
 root.mainloop()
